f45b0590e7405e7f08842f1ba11c5acd6b23b21c
hui.wu <729530722@qq.com>
2020-01-11 19:23:08 +0800

    ceshiw

---
+++ src/main/java/com/zero/util/test.java
@@ -0,0 +1,10 @@
+package com.zero.util;
+
+public class test {
+    public void test1(){
+        System.out.println("111");
+        System.out.println("222");
+        System.out.println("222");
+        System.out.println("222");
+    }
+}
