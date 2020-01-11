2746dcb074381b766c8518ceb4dc8bf05b2cbee2
hui.wu <729530722@qq.com>
2020-01-11 19:30:32 +0800

    ceshi1

--- src/main/java/com/zero/util/test.java
+++ src/main/java/com/zero/util/test.java
@@ -4,7 +4,7 @@ public class test {
     public void test1(){
         System.out.println("111");
         System.out.println("222");
-        System.out.println("222");
-        System.out.println("222");
+        System.out.println("3333");
+        System.out.println("44444");
     }
 }
